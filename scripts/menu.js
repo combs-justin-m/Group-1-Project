a738783f7d0f3d31c560c77427980e78e3f3d6ca
Justin Combs <combs.justin.m@gmail.com>
2015-05-28 16:01:00 -0400

    menu populates

--- scripts/menu.js
+++ scripts/menu.js
@@ -16,3 +16,18 @@ $('.menuButton').on('click', function (){
 $('#reserveTable').on('click', function (){
   $('#reservationForm').html('<h2>Table Reserved!</h2>');
 });
+
+// Menu-1 data //
+var menuString, menuFunc;
+
+$.getJSON('http://private-anon-f64b0c0cc-restaurantapi.apiary-mock.com/menu-1', function(data){
+
+menuString = $('#menuData').html();
+menuFunc = _.template(menuString);
+
+  _.each(data, function (arr){
+    _.each(arr, function(a){
+      $('.menuList').append(menuFunc(a));
+    })
+  });
+});
