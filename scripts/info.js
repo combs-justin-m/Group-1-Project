11fa0a60f8e3611ba40b11d8d72e50943cfa9c39
Vi Pham <tvpham11@gmail.com>
2015-05-28 17:07:09 -0400

    Working on info boxes 1 & 2 API stuff.

--- scripts/info.js
+++ scripts/info.js
@@ -1,4 +1,30 @@
-{
-"id": 1,
-"menu_item_id": 25
-}
+//News
+
+var getNews = $.getJSON('http://private-anon-161c10f24-restaurantapi.apiary-mock.com/news/latest');
+var templateStringNews = $('#news').html();
+var templateFuncNews = _.template(templateStringNews);
+
+var divNews = $('#newsContainer');
+
+getNews.done(function (data) {
+  $('#newsContainer').html('<span>' + data.title + '</span><span>' + data.date_published + '</span><p>' + data.post + '</p>');
+});
+
+
+//Daily Special
+
+var getDailySpecial = $.getJSON('http://private-anon-161c10f24-restaurantapi.apiary-mock.com/menu/special');
+var templateStringDS = $('#dailySpecial').html();
+var templateFuncDS = _.template(templateStringDS);
+
+var divDS = $('#dailySpecContainer');
+
+getDailySpecial.done(function (data) {
+
+  _.each(data, function(r) {
+
+    divDS.append(templateFuncDS(r));
+
+  });
+
+});
