c2ed8c4c534364fc2aa078adb164842c85d54ff5
Vi Pham <tvpham11@gmail.com>
2015-05-29 14:51:43 -0400

    added rough logo and restaurant name and working on pulling in daily special API. pulling photos from Flickr

--- scripts/info.js
+++ scripts/info.js
@@ -1,4 +1,4 @@
-//News
+//News//
 
 var getNews = $.getJSON('http://private-anon-161c10f24-restaurantapi.apiary-mock.com/news/latest');
 var newsTemplate = $('#news').html();
@@ -7,13 +7,10 @@ var templateFuncNews = _.template(newsTemplate);
 var divNews = $('#newsContainer');
 
 getNews.done(function (data) {
-  console.log(templateFuncNews(data));
-  // divNews.append(templateFuncNews(data));
+  divNews.append(templateFuncNews(data));
 });
 
-
-
-//Daily Special
+//Daily Special//
 
 var getDailySpecial = $.getJSON('http://private-anon-161c10f24-restaurantapi.apiary-mock.com/menu/special');
 var dailySpecialTemplate = $('#dailySpecial').html();
@@ -21,6 +18,15 @@ var templateFuncDS = _.template(dailySpecialTemplate);
 
 var divDS = $('#dailySpecContainer');
 
+var specialID;
 getDailySpecial.done(function (data) {
-  divDS.append(templateFuncDS(data));
+  specialID = data.menu_item_id;
+});
+
+$.getJSON('http://private-anon-161c10f24-restaurantapi.apiary-mock.com/menu-1').done(function(data) {
+  data.entrees.filter(function(menuItem) {
+    if(menuItem.id == specialID) {
+      divDS.append(templateFuncNews(menuItem));
+    }
+  });
 });
